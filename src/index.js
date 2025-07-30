2f1cef0a9554795f273b408ad0aa67518e3c79b1
mdot2x3 <mdot2x3@gmail.com>
2025-07-30 15:44:42 -0700

    feat: design page and add button interaction
    build a simple html and css UI with imported icon
    buttons.
    
    add function displayMenu() to index.js that
    reveals the hidden menu ul list when any button is
    clicked.
    
    update README.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 // import css file for css-loader & style-loader
 import "./style.css";
-// webpack test code
-import { greeting } from "./greeting.js";
 
-console.log(greeting);
+function displayMenu() {
+  const buttons = document.querySelectorAll(".fa-solid");
+  const menu = document.querySelector(".menu");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      menu.classList.remove("hidden");
+    });
+  });
+}
+
+displayMenu();
