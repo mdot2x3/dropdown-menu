7975c5de2e15e44900d52d3a0f3eae1138ccf166
mdot2x3 <mdot2x3@gmail.com>
2025-07-30 17:40:49 -0700

    feat: design and attach dropdown menu to button
    add css styling to design the dropdown menu.
    
    add getBoundingClientRect() inside the
    displayMenu() function to measure the position of
    whichever button is clicked on the screen, and
    then set the menu's absolute positioning to have
    it appear aligned to the menu's top-right corner.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,12 @@ function displayMenu() {
 
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
+      // unhide menu first so it can be measured
       menu.classList.remove("hidden");
+      // use to get the clicked button's position on the screen
+      const rect = button.getBoundingClientRect();
+      menu.style.top = `${rect.bottom + window.scrollY}px`;
+      menu.style.left = `${rect.right + window.scrollX - menu.offsetWidth}px`;
     });
   });
 }
